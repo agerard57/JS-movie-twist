919641f0e7939868691efd0893aaffcee9533b8b
agerard57 <agerard57@protonmail.com>
2022-03-04 16:09:42 +0100

    feat: Add index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const input = prompt();
+import { dirname } from "path";
+import express, { Router } from "express";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+var app = express();
+var router = Router();
+var path = __dirname + "/src/";
+
+router.get("/", (_req, res) => {
+  res.sendFile(path + "index.html");
+});
+
+app.use("/", router);
+
+//Put bootstrap in a static folder to link it in index.html
+app.use("/dist", express.static(__dirname + "/node_modules/bootstrap/dist"));
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.info("Live at Port 3000");
+});
