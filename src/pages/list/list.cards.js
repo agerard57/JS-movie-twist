33f78db0ecb0830997e99370954be9d407b06f49
agerard57 <agerard57@protonmail.com>
2022-03-11 15:29:23 +0100

    refacto: Remake the list.card

--- src/pages/list/list.cards.js
+++ src/pages/list/list.cards.js
@@ -1,7 +1,7 @@
+const cards = document.querySelector("#cards");
+
 export const createCard = (element) => {
-  let cards = document.querySelector("#cards");
-  let div = document.createElement("div");
-  let imgUrl = (size) => {
+  const imgUrl = (size) => {
     // size chart found here: https://www.themoviedb.org/talk/5aeaaf56c3a3682ddf0010de
     // |  poster  | backdrop |
     // | :------: | :------: |
@@ -12,51 +12,75 @@ export const createCard = (element) => {
     // |   w500   | original |
     // |   w780   | -------- |
     // | original | -------- |
-    let baseUrl = "https://image.tmdb.org/t/p/";
+    const baseUrl = "https://image.tmdb.org/t/p/";
     return baseUrl + size + element.backdrop_path;
   };
-  let truncateValue = 50;
-  let shortenedStoryline =
+  const truncateValue = 60;
+  const shortenedStoryline =
     element.overview.length > truncateValue
       ? `${element.overview.substring(0, truncateValue)}...`
       : element.overview;
-  let formatDate = element.release_date.replace(new RegExp("-", "g"), "/");
-  const card = `
-          <div class="card bg-dark text-white">
-            <img class="card-img" src="${imgUrl("w780")}" alt="Card image" />
-            <div class="card-img-overlay">
-              <h5 class="card-title">${element.title}</h5>
-              <p class="card-text">${shortenedStoryline}</p>
-              <p class="card-text"> Release date: ${formatDate}</p>
+  const movieReleaseDate =
+    "Release date: " + element.release_date.replace(new RegExp("-", "g"), "/");
+  // card div
+  const card = document.createElement("div");
+  card.setAttribute("class", "card bg-dark text-white");
+  card.addEventListener("click", () => {
+    window.location.href = window.location.href.replace(
+      "/list",
+      `/id/${element.id}`
+    );
+  });
+
+  // banner image
+  const banner = document.createElement("img");
+  banner.setAttribute("class", "card-img");
+  banner.src = imgUrl("w780");
+  banner.alt = `${element.title} banner`;
+  card.appendChild(banner);
+
+  // card overlay div
+  const cardOverlay = document.createElement("div");
+  cardOverlay.setAttribute(
+    "class",
+    "card-img-overlay h-100 d-flex flex-column justify-content-end deez"
+  );
+  card.appendChild(cardOverlay);
+
+  // title
+  const title = document.createElement("h5");
+  title.setAttribute("class", "card-title");
+  title.innerText = element.title;
+  cardOverlay.appendChild(title);
+
+  // synopsis
+  const synopsis = document.createElement("p");
+  synopsis.setAttribute("class", "card-text");
+  synopsis.innerText = shortenedStoryline;
+  cardOverlay.appendChild(synopsis);
+
+  // release date
+  const releaseDate = document.createElement("p");
+  releaseDate.setAttribute("class", "card-text");
+  releaseDate.innerText = movieReleaseDate;
+  cardOverlay.appendChild(releaseDate);
+
+  cards.appendChild(card);
+
+  // Another way to implement this
+  /*
+    const card = `
+            <div class="card bg-dark text-white">
+              <img class="card-img" src="${imgUrl("w780")}" alt="Card image" />
+              <div class="card-img-overlay">
+                <h5 class="card-title">${element.title}</h5>
+                <p class="card-text">${shortenedStoryline}</p>
+                <p class="card-text">${movieReleaseDate}</p>
+              </div>
             </div>
-          </div>
-          `;
-  div.innerHTML = card;
-  div.classList.add("mx-2");
-  cards.appendChild(div);
+            `; 
+    div.innerHTML = card;   
+    div.classList.add("mx-2");
+    cards.appendChild(div);
+  */
 };
-
-/* 
-      
-      // console stuff
-      
-      movieDB.forEach(createCard);
-      
-      function createCard(element) {
-        // card
-        var card = document.createElement("div");
-        card.setAttribute("class", "movie-card");
-        card.style.backgroundImage = element.background;
-      
-        // title
-        var title = document.createElement("h1");
-        title.innerText = element.title;
-        card.appendChild(title);
-
-        // synopsis
-        var synopsis = document.createElement("p");
-        synopsis.innerText = element.synopsis;
-        card.appendChild(synopsis);
-      
-        document.body.appendChild(card);
-      } */
