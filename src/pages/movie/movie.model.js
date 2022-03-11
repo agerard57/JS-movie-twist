cc276cf489fe4e1af51af2f73c17deb9ec0450a4
GERARD Alexandre <agerard57@protonmail.com>
2022-03-11 20:54:16 +0100

    feat: Make the movie model work

--- src/pages/movie/movie.model.js
+++ src/pages/movie/movie.model.js
@@ -1,33 +1,45 @@
-const section = document.querySelector("section");
-const div = document.createElement("div");
+import { formatImageUrl } from "/assets/scripts/utils/formatImageUrl.js";
+import { formatReleaseDate } from "/assets/scripts/utils/formatReleaseDate.js";
 
 export const createMoviePage = (movie) => {
-  const card = `
-            <div class="titles">
-          <h3 class="title">${movie.title}</h3>
-          <h5 class="original-title">${movie.original_title}</h5>
-        </div>
-        <div class="metadata-container">
-          <span class="metadata-item item-date">2022</span>
-          <span class="item-separator">|</span>
-          <span class="metadata-item item-lang">jp</span>
-          <span class="item-separator">|</span>
-          <span class="metadata-item item-runningtime">1h 30m</span>
-          <span class="item-separator">|</span>
-          <span class="metadata-item item-note">8.3</span>
-        </div>
-        <div class="genres-container">
-          <span>Action</span> <span class="item-separator">|</span>
-          <span>Horror</span> <span class="item-separator">|</span>
-          <span>Thriller</span>
-        </div>
-        <span class="overview"
-          >A 17 year old finds out that his girlfriend is dying, so he sets out
-          to give her an entire life, in the last year she has left.</span
-        >
-            `;
-  div.innerHTML = card;
-  div.classList.add("info-container", "fixed-bottom");
-  section.appendChild(div);
-  console.log("deez");
+  // Poster
+  const poster = document.querySelector("#poster");
+  poster.src = formatImageUrl(movie.poster_path, "w342");
+
+  // Title
+  const title = document.querySelector("#title");
+  title.innerHTML = movie.title;
+
+  // Original title
+  const originalTitle = document.querySelector("#original-title");
+  originalTitle.innerHTML = movie.original_title;
+
+  // Date
+  const itemDate = document.querySelector("#item-date");
+  itemDate.innerHTML = formatReleaseDate(movie.release_date, "yearOnly");
+
+  // Film language
+  const itemLang = document.querySelector("#item-lang");
+  itemLang.innerHTML = movie.original_language.toUpperCase();
+
+  // Average note
+  const avgNote = document.querySelector("#item-avgnote");
+  avgNote.innerHTML = movie.vote_average;
+
+  // Overview
+  const overview = document.querySelector("#overview");
+  overview.innerHTML = movie.overview;
+
+  // Background banner
+  const bgBanner = document.querySelector("#bg-image");
+  bgBanner.style.backgroundImage = `url(${formatImageUrl(
+    movie.backdrop_path,
+    "original"
+  )})`;
 };
+
+/* TODO list 
+  - Movie genres (loop and whatnot)
+  - Average note in color
+  - See what data has to be displayed next
+  - Make the whole thing responsive! */
