f0f0bb075f36855fdb1970d3c7bf070bba0a896a
GERARD Alexandre <agerard57@protonmail.com>
2022-03-19 01:16:49 +0100

    feat: Add a message on home

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -3,8 +3,15 @@ import {
   getUserData,
 } from "/assets/scripts/utils/userDataStorage.js";
 
+const pageTitle = document.querySelector("#page-title");
 const authBtn = document.querySelector("#auth-button");
 
 storeUserData();
-if (getUserData("user") === null) authBtn.style.display = "initial";
-else authBtn.style.display = "none";
+
+if (getUserData("user") === null) {
+  authBtn.style.display = "initial";
+  pageTitle.innerHTML = "My Movie Database";
+} else {
+  authBtn.style.display = "none";
+  pageTitle.innerHTML = `Welcome back ${getUserData("user")}!`;
+}
