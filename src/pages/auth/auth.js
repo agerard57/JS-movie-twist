f3b182270344dbb8fef5d9f0f81e98fe3174f2a1
GERARD Alexandre <agerard57@protonmail.com>
2022-03-20 02:42:06 +0100

    feat: Add a message incase someone goes to login while logged

--- src/pages/auth/auth.js
+++ src/pages/auth/auth.js
@@ -1,3 +1,5 @@
+import { isUserLogged } from "/assets/scripts/utils/userDataStorage.js";
+
 const qs = new URLSearchParams(window.location.search);
 const radioButtons = document.querySelectorAll("input[name='btnradio']");
 const signupForm = document.querySelector("#signup-form");
@@ -9,6 +11,30 @@ const switchVisibility = () => {
   loginForm.style.display =
     loginForm.style.display === "none" ? "block" : "none";
 };
+const loggedMessage = () => {
+  const parent = document.querySelector("body");
+  while (parent.firstChild) parent.firstChild.remove();
+
+  const divContainer = document.createElement("div");
+
+  const title = document.createElement("H1");
+  title.setAttribute("id", "page-title");
+  title.setAttribute("class", "d-flex justify-content-center");
+  title.innerHTML = "Already logged in";
+  divContainer.appendChild(title);
+
+  const redirectBtn = document.createElement("button");
+  redirectBtn.setAttribute("class", "btn btn-secondary");
+  redirectBtn.innerHTML = "Return to homepage";
+  redirectBtn.addEventListener("click", () => {
+    window.location.href = window.location.origin;
+  });
+  divContainer.appendChild(redirectBtn);
+
+  document.body.appendChild(divContainer);
+};
+
+if (isUserLogged) loggedMessage();
 
 for (const radioButton of radioButtons)
   radioButton.addEventListener("change", switchVisibility);
