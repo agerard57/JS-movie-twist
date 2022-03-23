d7cdbe53fcb22f96d69e382df8564c7d7c363330
GERARD Alexandre <agerard57@protonmail.com>
2022-03-23 15:16:34 +0100

    feat: Quick search

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const MoviesModel = require("../models/movies.model");
 
 module.exports = function routes(app) {
   const router = express.Router();
   const srcPath = __dirname.replace("routes", "");
 
   app.use(express.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
 
   app.use("/assets", express.static(srcPath + "assets"));
 
@@ -17,7 +20,43 @@ module.exports = function routes(app) {
   require("./movie.routes")(app, express, srcPath);
   require("./utils.routes")(app, express, srcPath);
 
-  /*   app.get("*", (_req, res) => {
+  //TODO CLEAN THIS MESS
+  app
+    .route("/search")
+    .get((_req, res) => {
+      res.render("search");
+    })
+    .post((req, res) => {
+      let resultItem = "";
+      let response = "";
+      let searchQuery = req.body.title;
+      if (searchQuery.length > 0)
+        MoviesModel.find(
+          { title: { $regex: new RegExp(searchQuery, "i") } },
+          (_err, results) => {
+            results.forEach((sResult) => {
+              const truncateValue = 30;
+              const shortenTitle =
+                sResult.title.length > truncateValue
+                  ? `${sResult.title.substring(0, truncateValue)}…`
+                  : sResult.title;
+
+              if (sResult.title.indexOf(searchQuery) !== -1)
+                resultItem =
+                  resultItem +
+                  `<li class="list-group-item"><a class="link-secondary" href="/movie/${sResult.id}">${shortenTitle}</a></li>`;
+            });
+            if (resultItem === "")
+              response = "<li class='list-group-item'>No movie found</li>";
+            else response = resultItem;
+            res.send({ response: response });
+          }
+        ).limit(6);
+    });
+};
+
+/*   app.get("*", (_req, res) => {
     res.redirect("/");
   }); */
-};
+//TODO Put this back on please
+//TODO² Don't forget to do a search page and new route page
