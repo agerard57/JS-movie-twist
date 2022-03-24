73e3dbd375f2c984a4924c71a911dc5285eee006
GERARD Alexandre <agerard57@protonmail.com>
2022-03-25 00:22:14 +0100

    refacto: Clean app.js file

--- src/server/normalizePort.js
+++ src/server/normalizePort.js
@@ -3,13 +3,12 @@
  *
  * Otherwise, if NaN, returns the default port (3000).
  *
- * @module utils
- * @param {val}           value Is the value prompted at the start of the command.
- * @return {number}       Returns the normalized port number OR 3000
+ * @param {String}        envPort Is the port prompted at the start of the command.
+ * @return {Number}       Returns the normalized port number OR 3000
  */
 
-module.exports = function normalizePort(val) {
-  let port = parseInt(val, 10);
+module.exports = function normalizePort(envPort) {
+  const port = parseInt(envPort, 10);
   if (port >= 0) return port;
   return 3000;
 };
